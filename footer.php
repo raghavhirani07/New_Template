2e02dedd45b068c488d64ebde80eac1464c1d597
Raghav <90678701+raghavhirani07@users.noreply.github.com>
2022-08-27 12:17:22 +0800

    Update Dynamic Footer
    Footer is static then change the footer text and fetch the text from database then do footer dynamic

--- footer.php
+++ footer.php
@@ -2,7 +2,16 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <span>© Copyright 2019 News | Powered by <a href="http://www.yahoobaba.net/">Yahoo Baba</a></span>
+                <span><?php
+                     $sql = "SELECT * FROM settings";
+                     $result = mysqli_query($conn, $sql) or die("Query Failed.");
+                     if(mysqli_num_rows($result) > 0){
+                         while($row = mysqli_fetch_assoc($result)) {
+
+                            echo $row['footerdesc'];
+                         }
+                     }
+                ?></span>
             </div>
         </div>
     </div>
